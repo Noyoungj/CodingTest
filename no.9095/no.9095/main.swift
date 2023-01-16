d645457601014783c5ec315c3c1a9f1ab15be305
YOUNGJAE NO <noyoungj96@gmail.com>
2023-01-16 17:29:45 +0900

    [ADD] no.9095

--- no.9095/no.9095/main.swift
+++ no.9095/no.9095/main.swift
@@ -7,5 +7,27 @@
 
 import Foundation
 
-print("Hello, World!")
+let read = Int(readLine()!)!
+let MAX = 11
 
+var result : [Int] = []
+
+for _ in 1...read {
+    let N = Int(readLine()!)!
+    
+    var dp = [Int](repeating: -1, count: MAX)
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+    
+    if N > 3 {
+        for i in 4...N {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+        }
+    }
+    result.append(dp[N])
+}
+
+for i in 0..<read {
+    print(result[i])
+}
