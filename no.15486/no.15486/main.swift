0452592fb9d738f7adae3a89a69c24e3c0db774a
YOUNGJAE NO <noyoungj96@gmail.com>
2023-01-25 23:12:05 +0900

    [ADD] no.15486

--- no.15486/no.15486/main.swift
+++ no.15486/no.15486/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
+let N = Int(readLine()!)!
+var arr = [[Int]](repeating: [Int](repeating: 0, count: 2), count: N + 2)
 
+for i in 1...N {
+    arr[i] = readLine()!.split(separator: " ").map{ Int($0)! }
+}
+
+var dp = [Int](repeating: 0, count: N + 2)
+
+var cM = 0
+
+for i in 1...(N + 1) {
+    cM = max(cM, dp[i])
+    if i + arr[i][0] > N + 1{
+        continue
+    }
+    dp[i + arr[i][0]] = max(dp[i + arr[i][0]], cM + arr[i][1])
+}
+
+print(dp[N + 1])
