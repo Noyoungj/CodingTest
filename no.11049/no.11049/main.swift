5547fb85bc97bf4840ee8fed5ed2fa85222f3b3d
YOUNGJAE NO <noyoungj96@gmail.com>
2023-05-12 01:23:23 +0900

    [UPDATE] no.11570

--- no.11049/no.11049/main.swift
+++ no.11049/no.11049/main.swift
@@ -2,11 +2,13 @@
 //  main.swift
 //  no.11049
 //
-//  Created by 노영재 on 2023/02/10.
+//  Created by 노영재 on 2023/05/11.
 //
 
 import Foundation
 
 let N = Int(readLine()!)!
 
-
+for i in 0..<N {
+    
+}
--- no.11570/no.11570/main.swift
+++ no.11570/no.11570/main.swift
@@ -8,29 +8,33 @@
 import Foundation
 
 let N = Int(readLine()!)!
-var arr = readLine()!.split(separator: " ").map{ Int($0)! }
-arr = [0] + arr
+var arr = [0] + readLine()!.split(separator: " ").map{ Int($0)! }
+
 var dp = [[Int]](repeating: [Int](repeating: Int.max, count: N + 1), count: N + 1)
-dp[0][0] = 0
-dp[1][0] = arr[1]
+
+dp[0][1] = 0
+dp[1][0] = 0
+
 for i in 0...N {
-    for j in i...N {
+    for j in 0...N {
+        if i == j { continue }
         
-
-        if i == j {
-            dp[i][j] = dp[0][j]
-            continue
-        }
+        let next = max(i, j) + 1
         
-        if i == 0{
-            dp[i][j] = dp[i][j - 1] + arr[j]
-            continue
-        }
+        if next > N { continue }
         
-
-        dp[i][j] = min(dp[i][j], dp[i][j - 1] + abs(arr[j] - arr[j - 1]))
-        dp[i][j] = min(dp[i][j], dp[i - 1][j] + abs(arr[i] - arr[i - 1]))
+        if i == 0 || j == 0 { arr[0] = arr[next] }
+        
+        dp[next][j] = min(dp[next][j], dp[i][j] + abs(arr[next] - arr[i]))
+        dp[i][next] = min(dp[i][next], dp[i][j] + abs(arr[next] - arr[j]))
     }
 }
 
-print(dp)
+var result = Int.max
+
+for i in 0...N {
+    result = min(result, dp[i][N])
+    result = min(result, dp[N][i])
+}
+
+print(result)
